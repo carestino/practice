6e293af47d1d6e4df35174176648c9ec2f4e7583
Nicolás Pardo <nicolaspar@gmail.com>
2023-05-12 11:56:06 -0300

    Co-authored-by: Nicolás Pardo <nico@carestino.com>

--- StepTwo/Question2.php
+++ StepTwo/Question2.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Implementá el método unique_names.
- * Cuando se pasan dos arrays deberá devolver un array que contenga los valores de ambos arrays unificados.
- * El array no debe tener duplicados.
+ * Recibe por parámetro dos arrays y deberá devolver un único array que contenga los valores unificados.
+ * El array que devuelve no debe tener duplicados.
  * Por ejemplo, al llamar a unique_names(['Ava', 'Emma', 'Olivia'], ['Olivia', 'Sophia', 'Emma']).
  * Devolvera ['Ava', 'Emma', 'Olivia','Sophia']
  */
